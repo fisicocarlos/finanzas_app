68d9e07f54f243ad8f46c0b1d6266c9e1b7d4c75
fisicocarlos <carlos.ramon.santonja@gmail.com>
2025-12-02 10:23:59 +0100

    refactor: Add formatters for jinja, css, js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,15 +1,14 @@
 let lastScroll = 0;
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
 
-window.addEventListener('scroll', () => {
-    const currentScroll = window.pageYOffset;
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
 
-    if (currentScroll > lastScroll && currentScroll > 50) {
-        nav.classList.add('hidden');
-    }
-    else if (currentScroll < lastScroll) {
-        nav.classList.remove('hidden');
-    }
+  if (currentScroll > lastScroll && currentScroll > 50) {
+    nav.classList.add("hidden");
+  } else if (currentScroll < lastScroll) {
+    nav.classList.remove("hidden");
+  }
 
-    lastScroll = currentScroll;
+  lastScroll = currentScroll;
 });
