ad0492cf52e5c7a3e78904b8d8f71f162753375f
fisicocarlos <carlos.ramon.santonja@gmail.com>
2025-12-02 09:35:59 +0100

    style: enhance navigation bar with fixed positioning and transition effects; add JavaScript for scroll behavior; update base template to include new script

---
+++ app/static/js/main.js
@@ -0,0 +1,15 @@
+let lastScroll = 0;
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll && currentScroll > 50) {
+        nav.classList.add('hidden');
+    }
+    else if (currentScroll < lastScroll) {
+        nav.classList.remove('hidden');
+    }
+
+    lastScroll = currentScroll;
+});
