521dbb5dd3d19ae2ad87e0fef9c15d5c618c2e45
fisicocarlos <carlos.ramon.santonja@gmail.com>
2025-12-29 14:59:47 +0100

    feat: Add log message for application URL in start script

--- start.sh
+++ start.sh
@@ -11,6 +11,7 @@ fi
 if ! pgrep -f "$PROCESS_NAME" > /dev/null ; then
     nohup python3 main.py > $LOG_FILE 2>&1 &
     echo "- Start app. Logs in $LOG_FILE"
+    echo "- Running on http://127.0.0.1:5000"
 else
     echo "- App already started. Doing nothing"
 fi
